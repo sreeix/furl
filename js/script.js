f52433c69711f2fa618636aff27e0138ad060036
V Sreekanth <sreeix@gmail.com>
2010-10-30 04:26:39 +0530

    Implementing save of queries

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
 $(document).ready(function(){
+  
+  // should have used the storage event api but heck it does not seem to work
+  var storedQueryDiv = $("#saved_requests ul");
+  for(var i =0; i< localStorage.length; i++){
+    var key = localStorage.key(i);
+    console.log(key);
+
+    if(key.indexOf(".url")!=-1) {
+      var url = localStorage[key];
+      console.log(key.substring(0, key.indexOf(".url")));
+      $("<li><a href='"+key.substring(0, key.indexOf(".url"))+"'>"+url+ "</a></li>").click(function(){
+        
+        var key_id = $(this).find("a").attr("href");
+        console.log(key_id+".html");
+        console.log(localStorage.getItem(key_id+".html"));
+        $("#api_request_form").html(localStorage.getItem(key_id+".html"));
+        return false;
+      }).appendTo(storedQueryDiv);
+    }  
+  }
+
   $("#api_request").focus(function(){
     if($(this).val().substring(0,1) !='/'){
       $(this).val("/"+$(this).val());
@@ -13,17 +34,18 @@ $(document).ready(function(){
     var requestType = $("#request_type option:selected").val();
     var dataType = $("#response_data_type option:selected").val();
     var queryParams ={};
+    $(".saved_requests").trigger("refresh");
     $(".params").each(function(index, elem){
       var key = $(elem).find(".query_param").val();
       if(typeof key !== 'undefined' && key && key !="") {
         queryParams[key] =  $(elem).find(".query_param_value").val()
       }
     });
-    console.log(queryParams);
-    console.log(requestType);
-    console.log(dataType);
     var requestUrl = $("#base_url").val()+ $("#api_request").val();
-    console.log(requestUrl);
+    var key = Math.round(Math.random()* 10000000);
+    localStorage[key +".url" ] =  requestUrl;
+    localStorage[key +".html"] =   $("#api_request_form").html();
+    
     var startTime = new Date();
     $(".status,#response_area").removeClass("error");
     $.ajax(
@@ -32,15 +54,11 @@ $(document).ready(function(){
         url: requestUrl, 
         data: queryParams,
         error: function(request, textStatus, errorCode){
-          console.log(request);
-          console.log(textStatus);
           $(".status").text(textStatus + "(Error code = "+ request.status+" )" + "(Status = "+ request.statusText+ " )").removeClass("invisible loading").addClass("error");
           $("#response_area").val(request.responseText).addClass("error");
         },
         success: function(data, textStatus, request ){
-          console.log(request);
           $("#response_area").val(request.responseText);
-          console.log(data);
           $(".status").removeClass("loading invisible");
         },
         complete: function(){
