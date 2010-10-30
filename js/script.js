12326d5da676b889db584b8beacaf30130115be2
V Sreekanth <sreeix@gmail.com>
2010-10-30 16:49:02 +0530

    Local storage sucks but it works

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+function bindFormElements(){
+  $("#api_request").focus(function(){
+    if($(this).val().substring(0,1) !='/'){
+      $(this).val("/"+$(this).val());
+    }
+  });
+  $("#add_params").click(function(){
+    $("#params_container .params:first").clone().appendTo("#params_container");
+    return false;
+  });
+}
+
+
 $(document).ready(function(){
   
   // should have used the storage event api but heck it does not seem to work
@@ -13,38 +26,52 @@ $(document).ready(function(){
         
         var key_id = $(this).find("a").attr("href");
         console.log(key_id+".html");
-        console.log(localStorage.getItem(key_id+".html"));
-        $("#api_request_form").html(localStorage.getItem(key_id+".html"));
+        console.log(localStorage[key_id+".html"]);
+        $("#api_request_form").html(localStorage[key_id+".html"]);
+        
+        $("#base_url").val(localStorage[key_id+".base_url"]);
+        $("#api_request").val(localStorage[key_id+".api_request"]);
+        $("#request_type").val(localStorage[key_id+".request_method"]);
+        $("#response_data_type").val(localStorage[key_id+".data_type"]);
+        $(".params").each(function(index, elem){
+          $(elem).find(".query_param").val(localStorage[key_id+"."+index+".key"]);
+          $(elem).find(".query_param_value").val(localStorage[key_id+"."+index+".value"]);
+        });
+        bindFormElements()
         return false;
       }).appendTo(storedQueryDiv);
     }  
   }
-
-  $("#api_request").focus(function(){
-    if($(this).val().substring(0,1) !='/'){
-      $(this).val("/"+$(this).val());
-    }
-  });
-  $("#add_params").click(function(){
-    $("#params_container .params:first").clone().appendTo("#params_container");
-    return false;
-  });
+  bindFormElements();
   $("#api_request_form").submit(function(){
-    $(".status").removeClass("invisible").addClass("loading");
+    var random_key = Math.round(Math.random()* 10000000);
     var requestType = $("#request_type option:selected").val();
     var dataType = $("#response_data_type option:selected").val();
     var queryParams ={};
+    var requestUrl = $("#base_url").val()+ $("#api_request").val();
+
+    $(".status").removeClass("invisible").addClass("loading");
+    
     $(".saved_requests").trigger("refresh");
     $(".params").each(function(index, elem){
       var key = $(elem).find(".query_param").val();
-      if(typeof key !== 'undefined' && key && key !="") {
-        queryParams[key] =  $(elem).find(".query_param_value").val()
+      var value = $(elem).find(".query_param_value").val();
+      if(typeof key !== 'undefined' && key && key !=="") {
+        queryParams[key] =  value;
       }
+      localStorage[random_key +"."+index+".key" ] =  key;
+      localStorage[random_key +"."+index+".value" ] =  value;
+      console.log(random_key +"."+index+".key", key, value);
     });
-    var requestUrl = $("#base_url").val()+ $("#api_request").val();
-    var key = Math.round(Math.random()* 10000000);
-    localStorage[key +".url" ] =  requestUrl;
-    localStorage[key +".html"] =   $("#api_request_form").html();
+
+    localStorage[random_key +".url" ] =  requestUrl;
+    localStorage[random_key +".base_url" ] =  $("#base_url").val();
+    localStorage[random_key +".api_request" ] =  $("#api_request").val();
+    localStorage[random_key +".request_method" ] =  requestType;
+    localStorage[random_key +".data_type" ] =  dataType;
+    
+    localStorage[random_key +".url" ] =  requestUrl;
+    localStorage[random_key +".html"] =   $("#api_request_form").html();
     
     var startTime = new Date();
     $(".status,#response_area").removeClass("error");
